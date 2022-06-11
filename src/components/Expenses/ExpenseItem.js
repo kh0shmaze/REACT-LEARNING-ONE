d0e5eaa23e6579c454eea1fbc62625906bd48662
unknown <kh0shmaze@gmail.com>
2022-06-11 21:21:08 +0430

    creatr ExpensesFilter js&css
    lifting the stateUp

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,16 @@
-import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
+import "./ExpensesFilter";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const [title, setTile] = useState(props.title);
-
-  const clickHandler = () => {
-    setTile("updated!!!!");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2> {title}</h2>
+        <h2> {props.title}</h2>
         <div className="expense-item__price"> {props.amount}</div>
       </div>
-      <button onClick={clickHandler}>change item</button>
     </Card>
   );
 }
